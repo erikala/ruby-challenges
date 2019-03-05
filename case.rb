c47ba6e36c3b9a10a58fb6406e6715e4d9c70f5c
Erika La <henriikka.laurola@gmail.com>
2019-03-05 22:22:46 +0100

    initial commit

---
+++ case.rb
@@ -0,0 +1,13 @@
+weather = 'sunny'
+case weather
+when 'cloudy'
+  puts "Wear a jacket"
+when 'rainy'
+  puts "Take an umbrella with you"
+when 'foggy'
+  puts "Wear a long shirt"
+when 'cold'
+  puts "Wear a scarf and a beanie"
+else
+  puts "Wear whatever you want!"
+end
