0be8354e931aa636e23ea5b6b0aa52af96687dfa
Erika La <henriikka.laurola@gmail.com>
2019-04-16 22:51:53 +0200

    made chatbot respond to certain questions

--- chatbot.rb
+++ chatbot.rb
@@ -10,17 +10,23 @@ channel  = '#BananaStrand1234'
 socket.puts "NICK #{nickname}"
 socket.puts "USER #{nickname} 0 * #{nickname}"
 socket.puts "JOIN #{channel}"
+socket.puts "PRIVMSG #{channel} :I am so happy to be here!"
 
 while message = socket.gets do
   puts message
 
   if message.match('^PING :')
-
     server = message.split(':').last
-
     puts "PONG #{server}"
-
     socket.puts "PONG #{server}"
+
+  elsif message.match('How are you?')
+    puts "PRIVMSG #{channel} :I'm great, thanks!"
+    socket.puts "PRIVMSG #{channel} :I'm great, thanks!"
+
+  elsif message.match('Who are you?')
+    puts "PRIVMSG #{channel} :I'm the Skillcrush chatbot!"
+    socket.puts "PRIVMSG #{channel} :I'm the Skillcrush chatbot!"
 
   end
 
